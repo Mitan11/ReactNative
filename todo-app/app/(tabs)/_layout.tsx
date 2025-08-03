877e35072063d71bcbbe09e8feebff7abd19dcd8
mitan11 <mitantank00@gmail.com>
2025-08-03 23:26:02 +0530

    Theme Implemented

--- todo-app/app/(tabs)/_layout.tsx
+++ todo-app/app/(tabs)/_layout.tsx
@@ -1,16 +1,18 @@
 import { Tabs } from "expo-router"
 import { Ionicons } from '@expo/vector-icons';
+import useTheme from "@/hooks/useTheme";
 
 const TabsLayout = () => {
+    const {colors} = useTheme();
     return (
         <Tabs
             screenOptions={{
-                tabBarInactiveTintColor: "#cbd5e1",
-                tabBarActiveTintColor: "#22c55e",
+                tabBarInactiveTintColor: colors.primary,
+                tabBarActiveTintColor: colors.textMuted,
                 tabBarStyle: {
-                    backgroundColor: "#1e293b",
+                    backgroundColor: colors.surface,
                     borderTopWidth: 1,
-                    borderTopColor: "yellow",
+                    borderTopColor: colors.border,
                     height: 90,
                     paddingBottom: 30,
                     paddingTop: 10,
